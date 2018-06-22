fe387d3252d77b1798051ce67bef664acbd13bdd
kadavr95 <kadavr95@gmail.com>
2018-06-22 16:46:42 +0300

    final batch

--- js/main.js
+++ js/main.js
@@ -45,7 +45,6 @@ function onConnect() {
 
     console.log("onConnect");
 
-    //client.subscribe("devices/lora/807B859020000258/bme280");
     client.subscribe("devices/lora/#");
 
 
@@ -59,16 +58,14 @@ function onConnectionLost(responseObject) {
 
 };
 
-function onMessageArrived(message) {	
-    //var motion = ' ';
-    //var temperature = ' ';
-    //var humidity = ' ';
+function onMessageArrived(message) {
 
+	try{
     var msg = message.payloadString;
     var obj = JSON.parse(msg);
     var devEUI = obj.status.devEUI;
+	
 
-    //alert(msg);
 
     if (devEUI == "807B859020000258") {
         var chk = document.getElementById('checkbox-room-1');
@@ -86,20 +83,17 @@ function onMessageArrived(message) {
             WorkWithData(obj, devEUI, 3);
         }
     }
-        
+	}catch(e){
+	}
+
 }
 
 function WorkWithData(obj, devEUI, numRoom) {
-    //var temperature = ' ';
-    //var humidity = ' ';
-    //var motion = ' ';
+
 
     var temperature = obj.data.temperature;
     var humidity = obj.data.humidity;
     var motion = obj.data.value;
-    if(motion = "1") motion = "Yes" 
-    else motion = "No";
-
     if (temperature != undefined) {
         document.getElementById(numRoom + "-3-1").innerHTML = document.getElementById(numRoom + "-2-1").innerHTML;
         document.getElementById(numRoom + "-2-1").innerHTML = document.getElementById(numRoom + "-1-1").innerHTML;
@@ -113,25 +107,22 @@ function WorkWithData(obj, devEUI, numRoom) {
 
         if (temperature > 120) {
             document.getElementById("room" + numRoom).style.backgroundColor = '#ec5341';
-            alert("Temperature in room " + numRoom + " is too hight!");
+            alert("Temperature in room " + numRoom + " is too high!");
         }
     }
 
-    if (motion != undefined) {
+    if (motion == 1 || motion == 0) {
         document.getElementById(numRoom + "-3-3").innerHTML = document.getElementById(numRoom + "-2-3").innerHTML;
         document.getElementById(numRoom + "-2-3").innerHTML = document.getElementById(numRoom + "-1-3").innerHTML;
         document.getElementById(numRoom + "-1-3").innerHTML = motion;
 
-        if (document.getElementById(numRoom + "-1-3").innerHTML = "No"
-            && document.getElementById(numRoom + "-2-3").innerHTML == "No"
-            && document.getElementById(numRoom + "-3-3").innerHTML == "No") {
+        if (document.getElementById(numRoom + "-1-3").innerHTML == "0" && document.getElementById(numRoom + "-2-3").innerHTML == "0" && document.getElementById(numRoom + "-3-3").innerHTML == "0") {
             document.getElementById("room" + numRoom).style.backgroundColor = '#ec5341';
             GpioSet251(devEUI);
             alert("There is no motion in room " + numRoom + " for a long period!");
         }
 
-        if (document.getElementById(numRoom + "-1-3").innerHTML = "No"
-            && document.getElementById(numRoom + "-2-3").innerHTML == "No") {
+        if (document.getElementById(numRoom + "-1-3").innerHTML == "0" && document.getElementById(numRoom + "-2-3").innerHTML == "0") {
             GpioSet251(devEUI);
             GpioGet24(devEUI);
         }
@@ -140,28 +131,26 @@ function WorkWithData(obj, devEUI, numRoom) {
 
 //make sound
 function GpioGet24(devEUI) {
-    var msgtext = "gpio get 24";
+    var msgtext = "get 24";
 
     message = new Paho.MQTT.Message(msgtext);
 
-   // message.destinationName = "devices/lora/807B859020000258/gpio";
-    message.destinationName = "devices/lora/"+ devEUI +"/gpio";
+    message.destinationName = "devices/lora/" + devEUI + "/gpio";
     client.send(message);
-    
+
 }
 
 //get motion
 function GpioSet251(devEUI) {
-    var msgtext = "gpio set 25 1";
+    var msgtext = "set 25 1";
 
     message = new Paho.MQTT.Message(msgtext);
 
-    // message.destinationName = "devices/lora/807B859020000258/gpio";
     message.destinationName = "devices/lora/" + devEUI + "/gpio";
     client.send(message);
 
-}      
 }
+
 
 
 function showModal(id) {
@@ -176,8 +165,8 @@ function hideModal(id)
 
 function showRoomModal(id, room) {
     document.getElementById(id).style.display = 'block';
-    document.getElementById("toggle-room-"+room).style.display = 'block';
-    document.getElementById("Table-"+room).style.display = 'table';
+    document.getElementById("toggle-room-" + room).style.display = 'block';
+    document.getElementById("Table-" + room).style.display = 'table';
     document.getElementById("room-info").setAttribute("onclick", "hideRoomModal('room-info','" + room + "'); return false;");
 }
 
@@ -185,8 +174,8 @@ function hideRoomModal(id, room)
 
 {
     document.getElementById(id).style.display = 'none';
-    document.getElementById("toggle-room-"+room).style.display = 'none';
-    document.getElementById("Table-"+room).style.display = 'none';
+    document.getElementById("toggle-room-" + room).style.display = 'none';
+    document.getElementById("Table-" + room).style.display = 'none';
 }
 
 function colorChange() {
@@ -194,22 +183,6 @@ function colorChange() {
 }
 
 function termsChecked(roomID) {
-	var newDir, newFile;
-	tizen.filesystem.resolve("documents", function(dir) 
-	    {
-	       newDir = dir.createDirectory("newDir");
-	       newFile = newDir.createFile("newFilePath.txt");
-	       newFile.openStream(
-	        "w",
-	        function(fs) {
-	        	 fs.write("test test test");
-	        	 fs.close();
-	        }, function(e) {
-	        	 console.log("Error " + e.message);
-	        }, "UTF-8");
-	    });
-	    
-	
     var chk = document.getElementById('checkbox-room-' + roomID);
     if (chk.checked) {
         document.getElementById("room" + roomID).style.backgroundColor = '#7abd53';
